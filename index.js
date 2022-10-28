97f135cbfb18200003aaed85a927803480a2f81e
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2022-10-28 22:39:47 +0800

    Add Player factory func, and allow players to add marks in a square

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 'use strict';
 
+function Player(marker) {
+	return {
+		marker,
+	};
+}
+
 const game = (() => {
 	const _squares = Array(9).fill(null);
+	const _xPlayer = Player('X');
+	const _oPlayer = Player('O');
+	let _activePlayer = _xPlayer;
 
-	// For testing purposes only.
-	_squares[0] = 'X';
-	_squares[1] = 'O';
-	_squares[4] = 'X';
-	_squares[8] = 'O';
+	function _changeActivePlayer() {
+		_activePlayer = _activePlayer === _xPlayer ? _oPlayer : _xPlayer;
+	}
+
+	function isSquareTaken(index) {
+		return !!_squares[index];
+	}
+
+	function updateSquare(index) {
+		_squares[index] = _activePlayer.marker;
+		display.updateSquare(index, _activePlayer.marker);
+		_changeActivePlayer();
+	}
 
 	function init() {
 		display.renderSquares(_squares);
 	}
 
 	return {
+		isSquareTaken,
+		updateSquare,
 		init,
 	};
 })();
@@ -21,25 +40,40 @@ const game = (() => {
 const display = (() => {
 	const _domSquares = document.querySelector('.js-squares');
 
-	function _getDomSquare(square) {
+	function _handleSquareClick(event) {
+		const index = event.target.dataset.index;
+		if (game.isSquareTaken(index)) {
+			return;
+		}
+		game.updateSquare(index);
+	}
+
+	function _getDomSquare(square, index) {
 		const domSquare = document.createElement('button');
 		domSquare.classList.add('square');
 		if (square) {
 			domSquare.classList.add(square);
 		}
 		domSquare.textContent = square;
-
+		domSquare.dataset.index = index;
+		domSquare.addEventListener('click', _handleSquareClick);
 		return domSquare;
 	}
 
+	function updateSquare(index, marker) {
+		const domSquare = _domSquares.children[index];
+		domSquare.textContent = marker;
+		domSquare.classList.add(marker);
+	}
+
 	function renderSquares(squares) {
-		for (const square of squares) {
-			const domSquare = _getDomSquare(square);
+		for (let i = 0; i < squares.length; i++) {
+			const domSquare = _getDomSquare(squares[i], i);
 			_domSquares.appendChild(domSquare);
 		}
 	}
 
-	return { renderSquares };
+	return { updateSquare, renderSquares };
 })();
 
 game.init();
